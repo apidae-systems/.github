4814c7ef6280329958425bdd9c825402fbad20a8
Mumtahin Farabi <mfarabi619@gmail.com>
2025-09-28 13:16:24 -0400

    build: overhaul workspace
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- apps/microvisor/devenv.nix
+++ apps/microvisor/devenv.nix
@@ -78,6 +78,9 @@ in
   packages =
     with pkgs;
     [
+      ninja
+      ccache
+      dfu-util
       pulumi-esc
     ]
     ++ lib.optionals (!(config.container.isBuilding or false)) [ glab ];
--- apps/microvisor/languages.nix
+++ apps/microvisor/languages.nix
@@ -6,7 +6,12 @@
   languages = {
     nix.enable = true;
     shell.enable = true;
+    cplusplus.enable = true;
     typescript.enable = true;
+    c = {
+      enable = true;
+      debugger = pkgs.gdb;
+    };
     javascript = {
       enable = true;
       pnpm.enable = true;
